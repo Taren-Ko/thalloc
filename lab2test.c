9f5c5055397528adcdedd95d2e71f8436d3a95fb
andn03 <andn03.an@gmail.com>
2016-10-28 13:14:18 -0400

    Add files via upload

--- lab2test.c
+++ lab2test.c
@@ -10,55 +10,19 @@ void testSet4();
 */
 
 int main() {
-  testSet1();
-  /*testSet2();
-	testSet3();
-	testSet4();
-  */
+  void *u = malloc(8);
+  void *v = malloc(68);
+  void *x = malloc(5);
+  void *y = malloc(33);
+  void *z = malloc(33);
+  // ex1: malloc() test
+  printf("Hello %p\n", u);
+  printf("Hello %p\n", v);
+  printf("Hello %p\n", x);
+  printf("Hello %p\n", y);
+  printf("Hello %p\n", z);
+  // void *a = malloc(32);
+  // printf("Hello %p\n", a);
+
   return (errno);
 }
-
-void testSet1(){
-  void *x;
-  int i = 1;
-  for (;i<=5;i++){
- 	x = malloc(i+4);
-  	printf("Ex1_test%d Hello %p\n", i, x);
-  }
-}
-/*void testSet2(){
-  void *x;
-  int i = 5;
-  int c = 1;
-  for (;i<=10;i++){
- 	x = malloc(i);
- 	free(x);
-  	printf("Ex2_test%d Hello %p\n", c, x);
- 	c++;
-  }
-}
-*/
-/*void testSet3(){
-  void *x;
-  int i = 5;
-  int c = 1;
-  for (;i<=10;i++){
- 	x = malloc(i);
- 	free(x);
-  	printf("Ex3_test%d Hello %p\n", c, x);
- 	c++;
-  }
-}
-*/
-/*void testSet2(){
-  void *x;
-  int i = 5;
-  int c = 1;
-  for (;i<=10;i++){
- 	x = malloc(i);
- 	free(x);
-  	printf("Ex2_test%d Hello %p\n", c, x);
- 	c++;
-  }
-}
-*/
